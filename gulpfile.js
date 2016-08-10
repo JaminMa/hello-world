51ca51cb76c86fbdcac37acd7649e4895db293af
Jamin Ma <dev.jamin@gmail.com>
2016-08-10 14:55:37 -0400

    Building and serving content

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
+var merge = require('merge-stream');
+var del = require('del');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['build']);
 
-gulp.task('build', function() {
-  browserify({
-    entries: ['./src/js/main.js'],
-    debug: true
-  })
-  .bundle()
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest('./'));
+gulp.task('build', ['clean'], function() {  
+  var makeBundle = browserify({
+      entries: ['src/js/main.js'],
+      debug: true
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('public'));
+
+  var copyIndex = gulp.src('index.html')
+    .pipe(gulp.dest('public'));
+
+  return merge(makeBundle, copyIndex);
+});
+
+gulp.task('clean', function(){
+  return del(['public/**/*']);
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Express server listening on port %d', port); 
+});
