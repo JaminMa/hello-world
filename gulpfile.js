dd94ab34a1646f46f220f8b77166b0d0108d94b3
Jamin Ma <dev.jamin@gmail.com>
2016-11-03 00:13:40 -0400

    Adding multiple build steps
    Updating the gulp configuration to build SASS files and perform
    minification.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,61 @@
 'use strict';
 
 var gulp = require('gulp');
-var merge = require('merge-stream');
+var gulpif = require('gulp-if');
+var concat = require('gulp-concat');
 var del = require('del');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
+var rename = require('gulp-rename');
 
-gulp.task('default', ['build']);
+var sourceDir = 'src/';
+var destDir = 'public/';
 
-gulp.task('build', ['clean'], function() {  
-  var makeBundle = browserify({
-      entries: ['src/js/main.js'],
-      debug: process.env.ENVIRONMENT !== 'production'
+var isProduction = process.env.ENVIRONMENT === 'production';
+
+gulp.task('default', ['build-html', 'build-js', 'build-css']);
+
+gulp.task('clean', function(){
+  return del([destDir + '**/*']);
+});
+
+gulp.task('build-html', function() {
+  var useref = require('gulp-useref');
+  del.sync([destDir + '**/*.html']);
+  return gulp.src('index.html')
+  .pipe(gulpif(isProduction, useref({ noAssets: true })))
+  .pipe(gulp.dest(destDir));
+});
+
+gulp.task('build-js', function() {
+  var browserify = require('browserify');
+  var source = require('vinyl-source-stream');
+  var buffer = require('vinyl-buffer');
+  var uglify = require('gulp-uglify');
+  del.sync([destDir + '**/*.js']);
+  return browserify({
+      entries: [sourceDir + 'js/main.js'],
+      debug: !isProduction
     })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('public'));
-
-  var copyIndex = gulp.src('index.html')
-    .pipe(gulp.dest('public'));
-
-  return merge(makeBundle, copyIndex);
+    .pipe(gulpif(isProduction, buffer()))
+    .pipe(gulpif(isProduction, uglify()))
+    .pipe(gulpif(isProduction, rename('bundle.min.js')))
+    .pipe(gulp.dest(destDir));
 });
 
-gulp.task('clean', function(){
-  return del(['public/**/*']);
+gulp.task('build-css', function() {
+  var sourcemaps = require('gulp-sourcemaps');
+  var sass = require('gulp-sass');
+  var postcss = require('gulp-postcss');
+  var autoprefixer = require('autoprefixer');
+  var cleanCSS = require('gulp-clean-css');
+  del.sync([destDir + '**/*.css']);
+  return gulp.src(sourceDir + 'scss/**/*.scss', {base: '.'})
+    .pipe(gulpif(!isProduction, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulpif(isProduction, cleanCSS()))
+    .pipe(gulpif(isProduction, concat('styles.min.css'), concat('styles.css')))
+    .pipe(gulpif(!isProduction, sourcemaps.write()))
+    .pipe(gulp.dest(destDir));
 });
